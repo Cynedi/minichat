be82e8769c815b6588242f4b73cb84bb68250db8
cindy <cindy.nicolay@laposte.net>
2017-07-24 09:51:41 +0200

    fonction clik au bouton envoyer et recuperation du formulaire en jQuery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+$(function() {
+  monChat();
+//fonction au click sur le bouton envoyer//
+  $('#send').click(function(){ //On recupère le bouton envoyer//
+    var nom=$('#nom').val();//On récupère l'input du pseudo et on modifie la valeur avec val//
+    var message=$('write').val();//On récupère la textarea et on modifie sa valeur avec val//
+  })
+});
