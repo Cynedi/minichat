3720446c213a778860adacf0af67b0e4b2c85333
cindy <cindy.nicolay@laposte.net>
2017-07-25 10:48:29 +0200

    css, html, js, rectifié

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
 //Mes variables//
-var nom ="";
-var pseudo =";"
+var ordinateur=['hello', 'How are you?', 'Do you want to brush me?','Do you want to drink with me?', 'why not?', 'why are you so stupid?','I must to go, my kittens are waiting for me?'];//tableau des réponses de l'ordinateur//
+var x=0;
 
-//On crée une fonction pour pouvoir modifier les champs d'entrés//
+//1-On crée une fonction pour pouvoir modifier les champs d'entrés//
 function defineChamps() {
-nom=$('#nom').val();//On récupère l'input du pseudo et on modifie la valeur avec val//
+pseudo=$('#pseudo').val();//On récupère l'input du pseudo et on modifie sa valeur avec val//
 message=$('#write').val();//On récupère la textarea et on modifie sa valeur avec val//
 }
 
-//On crée une fonction pour afficher la conversation//
+//2-On crée une fonction pour afficher la conversation//
 function monChat(){
-  if(nom!=""&& message!=""){ // si les champs sont remplis//
-  $('#conversation').append("<p>" + nom + " " + 'dit:'+ " " + message+ "</p>"); //ajout texte dans ma fenetre de conversation//
+  if(pseudo!=""&& message!=""){ // si les champs sont remplis//
+  $('#conversation').append("<p>" + pseudo+ " " + 'dit:'+ " " + message+ "</p>"); //affichage de mon pseudo et de mon message dans la fenetre//
 }
 }
 
-//fonction au click sur le bouton envoyer//
+//3-fonction au click sur le bouton envoyer//
   $('#send').click(function()  {
+
+    x++; //On icremente le tableau des réponses de l'ordinateur//
+    ordinateur [x];//On appelle une des réponses du tableau//
+
     defineChamps(); //On appelle la fonction qui recupère mes champs d'entrées//
-    setTimeout(monChat,2000); //Toute les 3 secondes envoie du texte//
+
+//4-On crée une fonction qui se déclenche toute les 2 secondes pour afficher la réponse de l'ordi//
+    setTimeout(function(){$('#conversation').append("<p>" +'Mon Chat dit:' +" "+ ordinateur[x]+"</p>");},2000)
+
+    monChat() //On appelle la fonction qui affiche mon pseudo et message//
+
+    $('#write').val("");//On efface mon message envoyer de mon textarea//
 })
